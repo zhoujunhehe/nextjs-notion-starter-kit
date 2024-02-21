918446f5574656e3481380f702460e5b94939a02
zhoujun <197652334@qq.com>
2024-02-21 10:56:17 +0800

    site info

--- site.config.ts
+++ site.config.ts
@@ -14,12 +14,12 @@ export default siteConfig({
   author: 'Zhoujun',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '个人blog',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  // twitter: 'transitive_bs',
+  // github: 'transitive-bullshit',
+  // linkedin: 'fisch2',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
